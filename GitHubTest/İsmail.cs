3f1f420a3788a3d3415340d4f465e942cff77c39
ismail cadr <omeqa-splash@hotmail.com>
2018-12-10 21:59:54 +0300

    addClass

---
+++ GitHubTest/İsmail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    class İsmail
+    {
+
+        public string asdas { get; set; }
+        public int Yas { get; set; }
+    }
+}
